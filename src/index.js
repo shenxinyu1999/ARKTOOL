98fc75c544e3efec82d6e0bf21af9a5bdc471705
Fisher Shen <shenxinyu1999@outlook.com>
2020-12-27 00:27:01 -0500

    Remove unused files, reorganize files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
+import './css/index.css';
 import Material from './Material';
 
 ReactDOM.render(
